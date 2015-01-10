c54efb6f7bfd96d583d3a1114a7c89d89faf2491
enjoy2000 <enjoy3013@gmail.com>
2015-01-10 22:41:50 +0700

    Add static pages for the ministry and about iraq

---
+++ module/Landing/src/Landing/Controller/AboutIraqController.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Landing\Controller;
+
+use Application\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class AboutIraqController extends AbstractActionController
+{
+    public function indexAction()
+    {
+        return new ViewModel([]);
+    }
+
+    public function investmentAction()
+    {
+        return new ViewModel([]);
+    }
+
+    public function ministryDepartmentsAction()
+    {
+        return new ViewModel([]);
+    }
+
+    public function faqAction()
+    {
+        return new ViewModel([]);
+    }
+
+    public function speechesInterviewsAction()
+    {
+        return new ViewModel([]);
+    }
+
+    public function undersecretariesAction()
+    {
+        return new ViewModel([]);
+    }
+
+    public function announcementsAction()
+    {
+        return new ViewModel([]);
+    }
+
+    public function magazineAction()
+    {
+        return new ViewModel([]);
+    }
+}
