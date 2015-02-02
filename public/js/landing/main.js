4b244274838f3aa81b26858cd5317f865f680082
Hat Dao <hat.dao@lazada.com>
2015-02-02 13:55:04 +0700

    Add according menu

--- public/js/landing/main.js
+++ public/js/landing/main.js
@@ -1,43 +1,45 @@
 /**
  * Created by antiprovn on 1/3/15.
  */
-$(document).ready(function(){
-    /**
-     * Effect for main menu
-     */
-    $('#top-nav .dropdown').hover(
-        function(){
-            $(this).addClass('open active');
-            $(this).find('.toggle-dropdown').dropdown('toggle');
-        },
-        function(){
-            $(this).removeClass('open active');
-            $(this).find('.toggle-dropdown').dropdown('toggle');
+(function($){
+    $(document).ready(function(){
+        /**
+         * Effect for main menu
+         */
+        $('#top-nav .dropdown').hover(
+            function(){
+                $(this).addClass('open active');
+                $(this).find('.toggle-dropdown').dropdown('toggle');
+            },
+            function(){
+                $(this).removeClass('open active');
+                $(this).find('.toggle-dropdown').dropdown('toggle');
 
-            // for current menu item
-            if ($(this).hasClass('current-menu-item')) {
-                $(this).addClass('active');
+                // for current menu item
+                if ($(this).hasClass('current-menu-item')) {
+                    $(this).addClass('active');
+                }
             }
-        }
-    );
-    $('#top-nav .dropdown > a').click(function(e){
-        e.preventDefault();
+        );
+        $('#top-nav .dropdown > a').click(function(e){
+            e.preventDefault();
 
-        window.location.href = $(this).attr('href');
-    });
+            window.location.href = $(this).attr('href');
+        });
 
-    // news ticker
-    $.get(newsTickerUrl, function($data){
-        $('.news-ticker').html($data);
-    });
+        // news ticker
+        $.get(newsTickerUrl, function($data){
+            $('.news-ticker').html($data);
+        });
 
-    $('.news-ticker').newsTicker({
-        row_height: 28,
-        max_rows: 1,
-        speed: 600,
-        direction: 'up',
-        duration: 4000,
-        autostart: 1,
-        pauseOnHover: 1
+        $('.news-ticker').newsTicker({
+            row_height: 28,
+            max_rows: 1,
+            speed: 600,
+            direction: 'up',
+            duration: 4000,
+            autostart: 1,
+            pauseOnHover: 1
+        });
     });
-});
+})(jQuery);
