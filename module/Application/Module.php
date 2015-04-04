a494e34133a341fb0c251950bd2491d0713a4150
enjoy2000 <enjoy3013@gmail.com>
2015-04-04 19:49:05 +0000

    Change timezone to Baghdad

--- module/Application/Module.php
+++ module/Application/Module.php
@@ -20,7 +20,7 @@ class Module
         /**
          * Set default time zone
          */
-        date_default_timezone_set('Asia/Dhaka');
+        date_default_timezone_set('Asia/Baghdad');
 
 
         // session container
